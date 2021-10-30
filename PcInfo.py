7e1e226cf3a45e0b5dd6c0fe59b15e4a0aeb2f53
3faze <93428357+3faze@users.noreply.github.com>
2021-10-30 12:18:03 +0100

    Add files via upload

---
+++ PcInfo.py
@@ -0,0 +1,19 @@
+question = input("What Component or Periferic are u looking for?: ")
+
+if question == "GPU":
+    print("GeForce GTX 1650")
+
+elif question == "CPU":
+    print("10th Generation Intel Core i3")
+
+elif question == "Ram" or question == "ram":
+    print("This pc has 16GB of ram")
+
+elif question == "Mouse" or question == "mouse":
+    print("Katar Pro XT")
+
+elif question == "Keyboard":
+    print("Mekan TKL: Red switches")
+
+
+    
